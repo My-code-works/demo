c289b8d4e9ae35340d2496a1d2ec7fe632aa28ed
Qingsong Lv <lqs@mail.bnu.edu.cn>
2019-01-24 10:48:05 +0800

    update

--- backend/text2sec/views.py
+++ backend/text2sec/views.py
@@ -10,9 +10,50 @@ def JsonResponse(dic, format=False):
         return HttpResponse(json.dumps(dic, ensure_ascii=False), content_type="application/json")
 
 
+def get_labels(para):
+    return ['B0', '']
+
+
+def wraph(lev_list):
+    return '<h%d>'%(siz) + '.'.join([str(x) for x in lev_list]) + '</h%d>\n'%(len(lev_list))
+
+
+def wrapp(text):
+    return '<p>' + text + '</p>\n'
+
+
+def labeled2html(para, tag):
+    html = ''
+    id = range(len(para))
+    pre = [-1]
+    for i, p, t in zip(id, para, tag):
+        if t[0] == 'B':
+            change = int(t[1:])
+            if change > 0:
+                for i in range(change):
+                    pre.append(1)
+            elif change < 0:
+                for i in range(change):
+                    pre = pre[:1]
+            pre[-1] += 1
+            if p[0] != 0:
+                html += wraph(pre, now_dep+1)
+        html += wrapp(p)
+    return html
+
+
+def para2tag(para):
+    return get_labels(para)
+
+
+def text2para(text):
+    return text.split('\n')
+
+
 @csrf_exempt
 def text2sec(request):
     text = json.loads(request.body)["text"]
-    html = "<h1>" + text + "</h1>\n"
-    html += 'hello'
+    para = text2para(text)
+    tag = para2tag(para)
+    html = labeled2html(para, tag)
     return JsonResponse({'result': html})
